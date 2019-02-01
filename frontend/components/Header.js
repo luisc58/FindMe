4641030c6daea163cd1668ec954388faf19955c3
luis <luis.scholars@gmail.com>
2019-02-01 03:39:14 -0500

    initial commit

---
+++ frontend/components/Header.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+const Header = props => (
+    <StyledHeader>
+        
+    </StyledHeader>
+)
+
+export default Header;
---
+++ frontend/components/Page.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import Header from './Header';
+import Meta from './Meta';
+
+const theme = {
+    black: '#393939'
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+class Page extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta />
+                    <Header />
+                    <Inner>{this.props.children}</Inner>
+                </StyledPage>
+            </ThemeProvider>
+        );
+    }
+}
+
+export default Page;
